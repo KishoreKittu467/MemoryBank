bb5d8d3a98d50b2caa4b4d3fc0c35b57775add7b
Kishore Kittu <mummana.saikishore@thoughtworks.com>
2020-06-16 02:49:32 +0530

    [Kishore]Add model classes and basic compute methods

---
+++ app/src/main/java/com/kkapps/memorybank/models/Element.kt
@@ -0,0 +1,26 @@
+package com.kkapps.memorybank.models
+
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import com.kkapps.memorybank.utils.Constants
+
+data class Element(
+    @ColumnInfo(name = "element_type") var elementType: FieldType,
+    @ColumnInfo(name = "created_time") var createdTime: Long = 0,
+    @ColumnInfo(name = "start_time") var startTime: Long = 0,
+    @ColumnInfo(name = "last_modified") var lastModified: Long = 0,
+    @ColumnInfo(name = "title") var title: String = "",
+    @ColumnInfo(name = "is_marked") var isMarked: Boolean = false,
+    @ColumnInfo(name = "color") var color: Int = 0,
+    @ColumnInfo(name = "text") var text: CharSequence? = "",
+    @ColumnInfo(name = "gallery") var gallery: Set<Image>? = null,
+    @ColumnInfo(name = "repeat") var repeat: List<Recursion>? = null,
+    @ColumnInfo(name = "tags") var tags: MutableSet<Entity>? = null,/** setOf(Entity("#tagName1"), Entity("#tagName2")) */
+    @ColumnInfo(name = "people") var people: MutableSet<Entity>? = null,/** setOf(Entity(personOneId, personOneName, Image(Constants.MY_DP_1)), Entity(Entity(personTwoId, personTwoName, Image(Constants.MY_DP_2)) */
+    @ColumnInfo(name = "contributors") var contributors: MutableSet<Entity>? = null,/** setOf(Entity(personOneId, personOneName, Image(Constants.MY_DP_1)), Entity(Entity(personTwoId, personTwoName, Image(Constants.MY_DP_2)) */
+    @ColumnInfo(name = "ownerId") var ownerId: String = Constants.MY_ID
+) {
+    @ColumnInfo(name = "id")
+    @PrimaryKey
+    var id: String = "$elementType $createdTime"
+}
--- app/src/main/java/com/kkapps/memorybank/ui/main/HomeActivity.kt
+++ app/src/main/java/com/kkapps/memorybank/ui/main/HomeActivity.kt
@@ -1,29 +1,25 @@
-package com.kkapps.memorybank
+package com.kkapps.memorybank.ui.main
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.kkapps.memorybank.ui.main.SectionsPagerAdapter
+import com.kkapps.memorybank.R
+import kotlinx.android.synthetic.main.activity_main.viewPager
+import kotlinx.android.synthetic.main.activity_main.fab
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Hi There!", Snackbar.LENGTH_SHORT)
                 .setAction("Action", null).show()
         }
     }
